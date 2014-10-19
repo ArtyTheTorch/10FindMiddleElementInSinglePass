fa16eaed5370bdf5beedb49d33fae24e785d9ab2
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:45:17 -0700

    Returns the middle node of a linked list
    Fun with Linked lists and linkedlistnodes
    UI-Implemntation

---
+++ 10FindMiddleElementInSinglePass/FindMiddle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10FindMiddleElementInSinglePass
+{
+    class FindMiddle
+    {
+        LinkedList<object> list = new LinkedList<object>();
+        public FindMiddle(string[] p)
+        {
+            for (int i = 0; i < p.Length; i++)
+            {
+                list.AddLast(Convert.ToInt32(p[i]));
+            }
+        }
+
+       public string getMiddle()
+        {
+           LinkedListNode<object> current = list.First;
+           LinkedListNode<object> everyother = list.First;
+           //have every other go twice as fast as current
+           everyother = everyother.Next;
+           int count = 0;
+           while (everyother != null)
+           {
+               count++;
+               current = current.Next;
+               everyother = everyother.Next;
+               if (everyother != null)
+               {
+                   everyother = everyother.Next;
+               }
+               }
+           return current.Value.ToString();
+        }
+     }
+}
---
+++ 10FindMiddleElementInSinglePass/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _10FindMiddleElementInSinglePass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FindMiddle x = new FindMiddle(textBox1.Text.Split(','));
+            label2.Text = x.getMiddle();
+        }
+    }
+}
